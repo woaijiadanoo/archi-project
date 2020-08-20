c6530dfc54976f505589acb89be8e2eedac47e32
jiangzh <3078956183@qq.com>
2020-08-20 11:51:56 +0800

    first commit

---
+++ src/main/java/com/sishuok/jiangzh/archi/ArchiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sishuok.jiangzh.archi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sishuok/jiangzh/archi/ArchiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sishuok.jiangzh.archi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
