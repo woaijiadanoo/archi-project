314d68cf82c8b11b821b21ef9c1ea8ccc50761ff
jiangzh <jiangzh@sina.com>
2020-10-25 21:24:10 +0800

    8-23 权限模块-Dispatcher 条件查询实现02

--- src/main/java/com/sishuok/jiangzh/archi/shopping/permit/spi/dispatcher/dao/DispatcherDAOImpl.java
+++ src/main/java/com/sishuok/jiangzh/archi/shopping/permit/spi/dispatcher/dao/DispatcherDAOImpl.java
@@ -30,17 +30,53 @@ public class DispatcherDAOImpl<M extends BaseVO> extends DispatcherDAOTemplate<M
         System.out.println("name = " + name);
         if(name.equals("ResourcePermitModel")){
             return resourcePerimitMatch(m,query);
+        }else if(name.equals("PermitModel")){
+            return permitModelMatch(m,query);
+        }else if(name.equals("ResourceDispatcherModel")){
+            return resourceDispatcherModel(m,query);
+        }else if(name.equals("ResourceModel")){
+            return resourceModel(m,query);
+        }else if(name.equals("RoleModel")){
+            return roleModel(m,query);
+        }else if(name.equals("UserModel")){
+            return userModel(m,query);
+        }else if(name.equals("UserRoleModel")){
+            return userRoleModel(m,query);
         }
         return false;
     }
 
     private boolean resourcePerimitMatch(M m,M q){
+
         return hasValue(m,q,"resourceId='") || hasValue(m,q,"permitId='");
+    }
+    private boolean permitModelMatch(M m,M q){
+        return hasValue(m,q,"permitStr='");
+    }
+    private boolean resourceDispatcherModel(M m,M q){
+        return hasValue(m,q,"ownerId='") || hasValue(m,q,"type=",",")|| hasValue(m,q,"resourcePermitId='");
+    }
+    private boolean resourceModel(M m,M q){
+        return hasValue(m,q,"resourceName='") || hasValue(m,q,"resourcePath='");
+    }
+    private boolean roleModel(M m,M q){
+        return hasValue(m,q,"showName='") || hasValue(m,q,"roleName='");
+    }
+    private boolean userModel(M m,M q){
+        return hasValue(m,q,"userName='");
+    }
+    private boolean userRoleModel(M m,M q){
+        return hasValue(m,q,"userId='") || hasValue(m,q,"roleId='");
     }
 
     private boolean hasValue(M m,M query,String beignStr){
+        return hasValue(m,query,beignStr,"'");
+    }
+
+
+    private boolean hasValue(M m,M query,String beignStr,String endStr){
         int beginIndex = m.toString().indexOf(beignStr) + beignStr.length();
-        int endIndex = beginIndex + m.toString().substring(beginIndex).indexOf("'");
+        int endIndex = beginIndex + m.toString().substring(beginIndex).indexOf("endStr");
 
         String value = m.toString().substring(beginIndex,endIndex);
         System.out.println("hasValue value = " + value);
